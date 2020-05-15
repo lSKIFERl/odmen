67fa2e14702b298a2abe3dd94a8053442f031250
lSKIFERl <skiferion@gmail.com>
2020-05-15 18:25:16 +0300

    wd

--- login.php
+++ login.php
@@ -11,7 +11,7 @@
 // Отправляем браузеру правильную кодировку,
 // файл login.php должен быть в кодировке UTF-8 без BOM.
 header('Content-Type: text/html; charset=UTF-8');
-if (session_start() && !empty($_COOKIE[session_name()])){
+if (!empty($_COOKIE[session_name()])){
 session_start();
 $_SESSION['csrf_token'] = substr( str_shuffle( 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM' ), 0, 10 );
 }
